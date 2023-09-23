086c0c60dc0da9dbe8db5f3ba889f280e49ee95c
tobias-barakaa <103282432+tobias-barakaa@users.noreply.github.com>
2023-09-23 16:36:43 +0300

    Update 2-c_route.py

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -23,11 +23,11 @@ def hello_hbnb():
     """
     return "Hello HBNB!"
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """
     Handle root = hbnb
-    
     when a clent acces the root it returns a message "HBNB"
 
     :return: A message string "HBNB
@@ -35,12 +35,10 @@ def hbnb():
     return "HBNB"
 
 
-
 @app.route('/c/<text>', strict_slashes=False)
 def text(text):
     """
     Handle root = text
-    
     when a clent acces the root it returns a message "HBNB"
 
     :return: A message string "{text}"
