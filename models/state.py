3051100023affc5ca16e97ec221ff95996060dea
tobias-barakaa <103282432+tobias-barakaa@users.noreply.github.com>
2023-09-25 10:03:59 +0300

    Update state.py

--- models/state.py
+++ models/state.py
@@ -1,25 +1,35 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
+import models
 from models.base_model import BaseModel, Base
+from models.city import City
+import sqlalchemy
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class State(BaseModel, Base):
-    """ State classs """
-    __tablename__ = 'states'  # Table name for SQLAlchemy
-
+    """ State class """
+    __tablename__ = "states"
     name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state")
 
-    # For DBStorage: Establish a one-to-many relationship with City
-    # If the State object is deleted, all linked objects are deleted.
-    cities = relationship('City', backref='state',
-                          cascade='all, delete-orphan', passive_deletes=True)
+    def __init__(self, *args, **kwargs):
+        """
+        init inherited
+        """
+        super().__init__(*args, **kwargs)
 
-    # For FileStorage: Create a getter attribute to return City state_id
-    @property
-    def cities(self):
-        from models import storage
-        all_cities = storage.all("City")
-        return [city for city in all_cities.values()
-                if city.state_id == self.id]
+    if models.storage_type != "db":
+        @property
+        def cities(self):
+            """getter for cities that return
+            a list of city instance equale to
+            curent state id
+            """
+            list_city = []
+            all_inst_c = models.storage.all(City)
+            for value in all_inst_c.values():
+                if value.state_id == self.id:
+                    list_city.append(value)
+            return list_city
