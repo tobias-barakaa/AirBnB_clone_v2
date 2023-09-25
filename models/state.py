f5577f3743a692d04b0e19dfe89bc5c1abf51d42
tobias-barakaa <103282432+tobias-barakaa@users.noreply.github.com>
2023-09-25 10:07:24 +0300

    Update state.py

--- models/state.py
+++ models/state.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+
+# Import necessary modules and classes
 import models
 from models.base_model import BaseModel, Base
 from models.city import City
@@ -7,25 +9,29 @@ import sqlalchemy
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 
-
 class State(BaseModel, Base):
     """ State class """
+    # Define the name of the table in the database
     __tablename__ = "states"
+    
+    # Define a column for the state's name, which cannot be empty
     name = Column(String(128), nullable=False)
+    
+    # Establish a one-to-many relationship with the City class
     cities = relationship("City", backref="state")
 
     def __init__(self, *args, **kwargs):
         """
-        init inherited
+        Constructor method for State class, inheriting from BaseModel.
+        Initializes object attributes with given arguments.
         """
         super().__init__(*args, **kwargs)
 
     if models.storage_type != "db":
         @property
         def cities(self):
-            """getter for cities that return
-            a list of city instance equale to
-            curent state id
+            """Getter method for cities that returns a list of City instances
+            associated with the current State instance.
             """
             list_city = []
             all_inst_c = models.storage.all(City)
